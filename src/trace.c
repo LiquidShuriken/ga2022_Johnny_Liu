14143626500b5c2ef86485700cc785703b2aca06
Johnny Liu <the_tough@outlook.com>
2022-10-19 15:38:05 -0400

    Resovled data racing

--- src/trace.c
+++ src/trace.c
@@ -1,5 +1,6 @@
 #include "trace.h"
 #include "heap.h"
+#include "mutex.h"
 #include "queue.h"
 #include "timer_object.h"
 
@@ -23,6 +24,7 @@ typedef struct trace_t
 	heap_t* heap;
 	queue_t* event_queue;
 	timer_object_t* timer;
+	mutex_t* mutex;
 	char* event_buffer;
 	char* path;
 	size_t event_capacity;
@@ -32,11 +34,12 @@ typedef struct trace_t
 trace_t* trace_create(heap_t* heap, int event_capacity)
 {
 	trace_t* trace = heap_alloc(heap, sizeof(trace_t), 8);
-	trace->event_queue = queue_create(heap, event_capacity);
 	trace->heap = heap;
-	trace->event_capacity = (size_t)event_capacity;
+	trace->event_queue = queue_create(heap, event_capacity);
 	trace->timer = timer_object_create(heap, NULL);
-	trace->event_buffer = calloc(trace->event_capacity * 80, sizeof(char));
+	trace->mutex = mutex_create(heap);
+	trace->event_capacity = (size_t)event_capacity;
+	trace->event_buffer = calloc(trace->event_capacity * 128, sizeof(char));
 	trace->capturing = false;
 	return trace;
 }
@@ -46,6 +49,7 @@ void trace_destroy(trace_t* trace)
 	queue_push(trace->event_queue, NULL);
 	queue_destroy(trace->event_queue);
 	timer_object_destroy(trace->timer);
+	mutex_destroy(trace->mutex);
 	free(trace->event_buffer);
 	heap_free(trace->heap, trace);
 }
@@ -57,9 +61,11 @@ void trace_duration_push(trace_t* trace, const char* name)
 	queue_push(trace->event_queue, e);
 	if (trace->capturing)
 	{
-		char info[80];
+		char info[128];
 		get_event_info(e, 'B', timer_object_get_ms(trace->timer), info);
-		strncat_s(trace->event_buffer, strlen(trace->event_buffer) + strlen(info) + 1 , info, strlen(info));
+		mutex_lock(trace->mutex);
+		strncat_s(trace->event_buffer, strlen(trace->event_buffer) + strlen(info) + 1, info, strlen(info));
+		mutex_unlock(trace->mutex);
 	}
 }
 
@@ -69,9 +75,11 @@ void trace_duration_pop(trace_t* trace)
 	event_info_t* e = queue_pop(trace->event_queue);
 	if (trace->capturing)
 	{
-		char info[80];
+		char info[128];
 		get_event_info(e, 'E', timer_object_get_ms(trace->timer), info);
-		strncat_s(trace->event_buffer, trace->event_capacity * 80, info, strlen(info));
+		mutex_lock(trace->mutex);
+		strncat_s(trace->event_buffer, trace->event_capacity * 128, info, strlen(info));
+		mutex_unlock(trace->mutex);
 	}
 
 	event_info_destroy(e);
@@ -81,8 +89,10 @@ void trace_capture_start(trace_t* trace, const char* path)
 {
 	trace->path = calloc(strlen(path) + 1, sizeof(char));
 	strncpy_s(trace->path, strlen(path) + 1, path, strlen(path));
-	char* header = "{\n\t\"displayTimeUnit\": \"ns\", \"traceEvents\" : [\n\t";
+	char* header = "{\n\t\"displayTimeUnit\": \"ns\", \"traceEvents\" : [\n";
+	mutex_lock(trace->mutex);
 	strncat_s(trace->event_buffer, strlen(trace->event_buffer) + strlen(header) + 1, header, strlen(header));
+	mutex_unlock(trace->mutex);
 	trace->capturing = true;
 }
 
@@ -90,6 +100,7 @@ void trace_capture_stop(trace_t* trace)
 {
 	trace->capturing = false;
 	char* tail = "\t]\n}";
+	mutex_lock(trace->mutex);
 	strncat_s(trace->event_buffer, strlen(trace->event_buffer) + strlen(tail) + 1, tail, strlen(tail));
 
 	wchar_t wide_path[1024];
@@ -113,6 +124,7 @@ void trace_capture_stop(trace_t* trace)
 		return;
 	}
 	CloseHandle(handle);
+	mutex_unlock(trace->mutex);
 }
 
 event_info_t* event_info_create(heap_t* heap, const char* name)
@@ -133,6 +145,6 @@ void event_info_destroy(event_info_t* event)
 
 void get_event_info(event_info_t* event, char ph, unsigned int tms, char* info)
 {
-	snprintf(info, 80, "{\"name\":\"%s\",\"ph\":\"%c\",\"pid\":0,\"tid\":\"%d\",\"ts\":\"%d\"},\n",
+	snprintf(info, 128, "\t\t{\"name\":\"%s\",\"ph\":\"%c\",\"pid\":0,\"tid\":\"%d\",\"ts\":\"%d\"},\n",
 		event->name, ph, event->tid, tms);
 }
